d8bf5ecb52e9711ad30bf8f647aed819663c4336
coolcatDev <alex_tsm@hotmail.com>
2015-02-20 00:40:46 +0200

    first commit

--- static/js/myscripts.js
+++ static/js/myscripts.js
@@ -7,7 +7,7 @@ document.onreadystatechange = function (){
 	
 	var page = window.location.toString();
 
-	if((page.indexOf('register') => -1) OR page.indexOf('editAccount') => -1)){
+	if((page.indexOf('register') > -1) OR (page.indexOf('editAccount') > -1)){
 		var subButton2 = document.getElementById("subButton2");
 		subButton2.onclick = function () {
 			var uploadBtn = document.getElementById("fileBtnHide");
