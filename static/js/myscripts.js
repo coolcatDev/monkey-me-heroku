2e2756b5549090824eebc52e5fc8c55c9d873c96
coolcatDev <alex_tsm@hotmail.com>
2015-02-20 02:56:56 +0200

    first commit

--- static/js/myscripts.js
+++ static/js/myscripts.js
@@ -42,12 +42,11 @@ document.onreadystatechange = function (){
 };
 
 function scrollTo(element, to, duration) {
-    if (duration < 0) return;
-    var difference = to - element.scrollTop;
-    var perTick = difference / duration * 2;
+	if (duration < 0) return;
+	var difference = to - element.scrollTop;
+	var perTick = difference / duration * 2;
 	setTimeout(function() {
-    	//element.scrollTop = element.scrollTop + perTick;
-    	window.scrollY = window.scrollY + perTick
-    	scrollTo(element, to, duration - 2); 
-		}, 10);
+		element.scrollTop = element.scrollTop + perTick;
+		scrollTo(element, to, duration - 2);
+	}, 10);
 };
